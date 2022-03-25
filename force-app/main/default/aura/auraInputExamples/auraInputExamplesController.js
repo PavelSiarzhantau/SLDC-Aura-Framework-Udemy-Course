f3e430955267fb7518d8ae9ec828b5ddf1653315
Pavel Siarzhantau <Pavel.Siarzhantau@gmail.com>
2022-03-25 18:37:39 +0300

    feat: add a lot of input fields to auraInputExamples.cmp

--- force-app/main/default/aura/auraInputExamples/auraInputExamplesController.js
+++ force-app/main/default/aura/auraInputExamples/auraInputExamplesController.js
@@ -1,3 +1,14 @@
 ({
-  myAction: function (component, event, helper) {}
+  handleKeyUp: function (cmp, evt) {
+    var isEnterKey = evt.keyCode === 13;
+    if (isEnterKey) {
+      var queryTerm = cmp.find("enter-search").get("v.value");
+      alert('Searched for "' + queryTerm + '"!');
+    }
+  },
+  writeConsole: function (cmp, evt) {
+    console.log(evt.getSource().get("v.value"));
+    console.log(evt.getSource());
+    console.log(evt.getSource());
+  }
 });
